a613d76dc95b88d29519b1842c1b6db505d1271c
Javi <javi_morales@hotmail.es>
2019-04-16 20:32:38 +0200

    Corregidas las librerias UART

--- DCO_TFG_PWM_bp/Src/main.c
+++ DCO_TFG_PWM_bp/Src/main.c
@@ -44,7 +44,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "math.h"
-#include "print_UART.h"
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
